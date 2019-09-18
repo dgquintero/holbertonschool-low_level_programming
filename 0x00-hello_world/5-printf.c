63d872d0b4a536ea736b95e1b247eeec2bf947bf
dgquintero <dgquintero02@hotmail.com>
2019-09-18 21:13:27 +0000

    5-printf.c answer

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+ * main - print a string using puts
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcomes is a piece of art,");
+	return (0);
+}
