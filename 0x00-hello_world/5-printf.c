96978554a6f17e1ea1b547b630428ec51014c474
dgquintero <dgquintero02@hotmail.com>
2019-09-18 21:17:21 +0000

    5-printf.c answer

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
+#include <stdio.h>
 /**
  * main - print a string using puts
  *
  * Return: Always 0 (Success)
  */
-#include <stdio.h>
 
 int main(void)
 {
-	printf("with proper grammar, but the outcomes is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcomes is a piece of art,\n");
+return (0);
 }
