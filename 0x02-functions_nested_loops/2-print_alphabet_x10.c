215084c5e54fd781d4b8d688059d5ecc72f003df
dgquintero <dgquintero02@hotmail.com>
2019-09-24 01:48:19 +0000

    3ans

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char n;
+	int d;
+	for (d = 0 ; d <= 10 ; d++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+	_putchar('\n');
+	}
+}
