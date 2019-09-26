56b80ea5ab73b69b6f999ab927d11e6ab1163063
dgquintero <dgquintero02@hotmail.com>
2019-09-26 18:01:33 +0000

    6 answer

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_line - check the code for Holberton School students.
+ *
+ * @n : parameter to check
+ *
+ * Return: not return.
+ */
+void print_line(int n)
+{
+	int d;
+	if (n > 0)
+	{
+		for (d = 0; d <= n; d++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
