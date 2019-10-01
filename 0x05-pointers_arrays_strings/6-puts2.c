c3135bb5b3bbee69a15392c54ff77d13ad5a76b4
dgquintero <dgquintero02@hotmail.com>
2019-10-01 19:19:48 +0000

    6 answer

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,7 +11,8 @@ void puts2(char *str)
 {
 	int n;
 
-	for (n = 0; str[n] != '\0'; n += 2)
-		_putchar(str[n]);
+	for (n = 0; str[n] != '\0'; n++)
+		if (str[n] % 2 == 0)
+			_putchar(str[n]);
 	_putchar('\n');
 }
