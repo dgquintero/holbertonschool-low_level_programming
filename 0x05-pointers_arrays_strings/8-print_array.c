252c5816ed7c2567370af77fd518c1527d70851d
dgquintero <dgquintero02@hotmail.com>
2019-10-01 20:35:13 +0000

    8 answer

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - print n elements of an aaray of integers, followed by a new line
+ *
+ * @a : is a pointer to a string
+ * @n : is a pointer to a string
+ * Return : not return.
+ */
+
+void print_array(int *a, int n)
+{
+	int d;
+
+	for (d = 0; d < n; d++)
+	{
+		if (d != n - 1)
+			printf("%i, ", a[d]);
+		else
+			printf("%i", a[d]);
+	}
+	printf ("\n");
+}
