f3cb95a1773bc5df4a007c0704b080a73c0c2372
dgquintero <dgquintero02@hotmail.com>
2019-10-03 19:52:29 +0000

    3 answer

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * _strcmp - function that concatenates two strings
+ *
+ * @s1 : parameter to check
+ * @s2 : parameter to check
+ *
+ * Return: a pointer to the serulting string dest.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int b, a;
+
+	for (b = 0; s1[b] != '\0'; b++)
+	{
+		if (s1[b] == s2[b])
+			a = s1[b] - s2[b];
+		else
+			a = s1[b] - s2[b];
+		break;
+	}
+	return (a);
+}
