bdd3f518938cb5307deabeeb60d3f65cdfe9d2d7
dgquintero <dgquintero02@hotmail.com>
2019-10-07 23:00:12 +0000

    2 answer

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,10 +11,10 @@ char *_strchr(char *s, char c)
 {
 	int a;
 
-	for (a = 0; *(s + a); a++)
+	for (a = 0; *(s + a) != '\0'; a++)
 	{
 		if (*(s + a) == c)
 			return (s + a);
 	}
-	return (s);
+	return (s + a);
 }
