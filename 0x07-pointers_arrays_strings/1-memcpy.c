6eeb9d3350b48bd294e26f5fdac4a027d2bc7c6e
dgquintero <dgquintero02@hotmail.com>
2019-10-07 19:04:40 +0000

    1 answer

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @dest: the address of memory to print
+ * @n: the size of the memory to print
+ * @src: the address of memory area to be copied
+ * Return: a pointer to the memory area.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+}
