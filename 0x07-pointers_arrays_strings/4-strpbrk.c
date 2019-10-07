31e56df0eed9e30765e8f2a20115c688b0759b76
dgquintero <dgquintero02@hotmail.com>
2019-10-07 23:56:03 +0000

    3answer

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
- * _strspn - gets the lenght of a prefix
- * @s: variable to check
- *
- * @accept: variable to check
- * Return: the bytes in the initial segment.
+ * *_memcpy - copies memory area
+ * @dest: the address of memory to print
+ * @n: the size of the memory to print
+ * @src: the address of memory area to be copied
+ * Return: a pointer to the memory area.
  */
 
 unsigned int _strspn(char *s, char *accept)
