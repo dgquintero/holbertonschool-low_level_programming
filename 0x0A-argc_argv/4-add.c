9535cf3b1f29a370fe576549db7075b463acee91
dgquintero <dgquintero02@hotmail.com>
2019-10-12 02:28:52 +0000

    an

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,18 +10,20 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j;
+	int i, j, d;
 
 	for (i = 0; i < argc; i++)
-		}
-		printf("0\n")
-		for (j= 0; argv[j] != '\0'; j++)
+	{
+		for (j= 0; argv[i][j] != '\0'; j++)
 		{
-			if ((argv[i][j] < 48 || argv[i][j] > 57 )
+			if (argv[i][j] < 48 || argv[i][j] > 57 )
 			{
 			    printf("Error\n");
 			    return (1);
 			}
-
-
+			d += atoi(arv[i]);
+		}
+	printf("%d\n", d);
+	}
+	return(0);
 }
