e4cb1b957d16707e8976df305a5f37d8bdf590d7
dgquintero <dgquintero02@hotmail.com>
2019-10-18 02:32:05 +0000

    4 answer

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _realloc - that reallocates a memory block using malloc and free
+ * @ptr: pointer
+ * @old_size : unsigned integer.
+ * @new_size : unsigned integer
+ * Return: a pointer to the duplicated string.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+	if (new_size == old_size)
+		return (NULL);
+	if (new_size == 0 || ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+}
