4d49fcbfbee2b47b03fa0e2c85292563dc3adf9b
dgquintero <dgquintero02@hotmail.com>
2019-10-22 01:27:50 +0000

    5 answer

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - that initialize a variable of type struct dog
+ * @name : The Name of the dog
+ * @age : the age of the dog
+ * @owner : the owner of the dog
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *new_dog;
+
+	new_dog = malloc(sizeof(struct dog));
+	if (new_dog == NULL)
+		return (NULL);
+	(*new_dog).name = name;
+	(*new_dog).age = age;
+	(*new_dog).owner = owner;
+	return (new_dog);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,7 @@ typedef struct dog
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif /* #ifndef _OBJECT_STRUCT_DOG_*/
