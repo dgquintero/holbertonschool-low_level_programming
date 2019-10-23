ff49901f1db9c6c78f9ff025ce09a234bd402fb9
dgquintero <dgquintero02@hotmail.com>
2019-10-23 17:29:21 +0000

    0answer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * print_name - prints a name as is
+ * @name: pointer to the name of the person
+ * @f : pointer to check.
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
