602fcf496f0648ae6cefb9d83fb1fe315aa0ea26
dgquintero <dgquintero02@hotmail.com>
2019-10-31 23:52:01 +0000

    2 answer

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_node - adds a new node at the beginning
+ * @head : const - list_t
+ * @str:
+ *
+ * Return: the number of notes.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *h;
+	unsigned int i = 0;
+
+	h = malloc(sizeof(list_t));
+	if (h == NULL)
+	{
+		free (h);
+		return (NULL);
+	}
+	h->str = strdup(str);
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	h->len = i;
+	h->next = *head;
+	*head = h;
+	return (h);
+}
