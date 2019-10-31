43ec09f283fcf5c41aa51b4984a1ef2057871771
dgquintero <dgquintero02@hotmail.com>
2019-10-31 21:26:24 +0000

    0 fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,22 +4,22 @@
 #include "lists.h"
 /**
  * print_list - function taht prints all the elements of a list
- * @h: const - list_t
+ * @h : const - list_t
  *
- * Return :  the number of notes
- * for Holberton project
+ *
+ * Return: the number of notes.
  */
 
 size_t print_list(const list_t *h)
 {
 	size_t i;
 
-	while ( h != NULL)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf ("[0] (nil)\n");
+			printf("[0] (nil)\n");
 		else
-			printf ("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 
 		h = h->next;
 		i++;
