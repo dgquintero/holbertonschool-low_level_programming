cec645d874eb9c32d25b3f049792bf34dca188f2
dgquintero <dgquintero02@hotmail.com>
2019-11-01 02:48:36 +0000

    1adv

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+  * first - function taht print.
+  * Return: nothing
+  */
+void first(void)
+{
+	 printf("You're beat! and yet, you must allow,\n");
+	  printf("I bore my house upon my back!\n");
+}
