38bb6d5953e57013cc2345983f9929b6d66bad35
dgquintero <dgquintero02@hotmail.com>
2019-11-04 18:10:36 +0000

    4 fixed

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+void free_list(list_t *head)
+{
+	list_t *new;
+	if(head != NULL)
+	{
+		while (head->next != NULL)
+		{
+			new = head->next;
+			free(head->str);
+			free(head);
+			head = new;
+		}
+	}
+		free(head->str);
+		free(head);
+}
