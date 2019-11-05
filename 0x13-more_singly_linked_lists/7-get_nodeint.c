3d1d8e8d77a78ecd1611c559807e102d4916589e
dgquintero <dgquintero02@hotmail.com>
2019-11-05 20:14:01 +0000

    7 answer

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,43 +4,20 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given position
- * @n : integer
- * @idx: index where the node should be added
+ * get_nodeint_at_index - returns the nth node
+ *
+ * @index: index where the node should be added
  * @head : pointer to a pointer
- * Return: the address of the new node.
+ * Return: returns the nth node
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *tail = *head, *node;
+	listint_t *tmp = head;
 
-	node = malloc(sizeof(listint_t));
-	if (node == NULL)
+	while (index > 0)
 	{
-		free(node);
-		return (NULL);
+		tmp = tmp->next;
+		index--;
 	}
-	node->n = n;
-	if (idx == 0)
-	{
-		node->next = *head;
-		*head = node;
-		return (node);
-	}
-	else
-	{
-		while ((idx - 1) > 0)
-		{
-			if (tail == NULL)
-			{
-				free(node);
-				return (NULL);
-			}
-			tail = tail->next;
-			idx--;
-		}
-		node->next = tail->next;
-		tail->next = node;
-		return (node);
-	}
+	return (tmp);
 }
