2163272b6529de768acfe43b57ab9e1518a9a00a
dgquintero <dgquintero02@hotmail.com>
2019-11-07 22:59:29 +0000

    0-answer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,10 +11,10 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	unsigned int sum = 0, n = 1;
 
-	for(i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '1' && b[i] != '0')
-			return(0);
+			return (0);
 	}
 	for (; i > 0; i--)
 	{
