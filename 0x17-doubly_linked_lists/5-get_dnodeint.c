666efe9c9fed1683f2b6da5f1b672efdc002b319
dgquintero <dgquintero02@hotmail.com>
2019-12-13 22:21:14 +0000

    5 answer

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -2,10 +2,24 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+
 /**
- * free_listint - function frees a listint_t list
- * @head : pointer - list_t
+ * get_dnodeint_at_index - returns the nth node
  *
- *
- * Return: Nothing.
+ * @index: index where the node should be added
+ * @head : pointer to a pointer
+ * Return: returns the nth node
  */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *tmp = head;
+
+	while (index > 0)
+	{
+		if (tmp == NULL)
+			return (NULL);
+		tmp = tmp->next;
+		index--;
+	}
+	return (tmp);
+}
