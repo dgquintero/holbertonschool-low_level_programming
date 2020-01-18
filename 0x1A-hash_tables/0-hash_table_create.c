df46d1f6fee911f5b109e758d8c4f7bee93ab4ee
dgquintero <dgquintero02@hotmail.com>
2020-01-18 23:02:33 +0000

    hash table project

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_create - function that creates a hach table
+ * @size: the size of the array
+ *
+ * Return: a struct hash_table_t type
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+	unsigned long int i;
+
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return NULL;
+
+	new_table->size = size;
+	new_table->array = malloc(size * sizeof(hash_node_t *));
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+	while(i < size)
+	{
+		new_table->array[i] = NULL;
+		i++;
+	}
+	return (new_table);
+}
