db03586b9ee7ae9fdf9527cad667c8eb5fed30f9
dgquintero <dgquintero02@hotmail.com>
2020-01-18 23:07:56 +0000

    test declaring str with null

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,12 +10,12 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
-	unsigned long int i;
+	hash_table_t *new_table = NULL;
+	/*unsigned long int i;*/
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
-		return NULL;
+		return (NULL);
 
 	new_table->size = size;
 	new_table->array = malloc(size * sizeof(hash_node_t *));
@@ -24,10 +24,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(new_table);
 		return (NULL);
 	}
-	while(i < size)
+	/**while (i < size)
 	{
 		new_table->array[i] = NULL;
 		i++;
-	}
+	}**/
 	return (new_table);
 }
