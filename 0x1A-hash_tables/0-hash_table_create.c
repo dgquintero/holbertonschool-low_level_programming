0f508375ad34c0d3c2af18929352694718ceb4dc
dgquintero <dgquintero02@hotmail.com>
2020-01-18 23:17:28 +0000

    0 task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,6 +11,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = NULL;
+	unsigned long int i;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
@@ -21,6 +22,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		free(new_table);
 		return (NULL);
+	}
+	while (i < size)
+	{
+		new_table->array[i] = NULL;
+		i++
 	}
 	return (new_table);
 }
