0f30e5d3253344284c5951d93e776aac5c9e7083
dgquintero <dgquintero02@hotmail.com>
2020-01-20 02:45:33 +0000

    hashtable get function

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_get - function that retrieves a value associated with a key
+ * @ht: the hash table you want to add or update
+ * @key: is the key
+ * Return: a struct hash_table_t type
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *tmp;
+	unsigned long int i;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	i = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[i];
+
+	if (tmp == NULL)
+		return (NULL);
+	for (; strcmp(tmp->key, key) != 0; tmp = tmp->next)
+		;
+	if (tmp == NULL)
+		return (NULL);
+
+	return (tmp->value);
+}
