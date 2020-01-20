d631ef50485a71b106327303fcd7d4f1a523e791
dgquintero <dgquintero02@hotmail.com>
2020-01-20 02:54:00 +0000

    hashtable get function test

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -17,11 +17,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	i = key_index((const unsigned char *)key, ht->size);
-	tmp = ht->array[i];
 
-	if (tmp == NULL)
-		return (NULL);
-	for (; strcmp(tmp->key, key) != 0; tmp = tmp->next)
+	for (tmp = ht->array[i]; strcmp(tmp->key, key) != 0; tmp = tmp->next)
 		;
 	if (tmp == NULL)
 		return (NULL);
