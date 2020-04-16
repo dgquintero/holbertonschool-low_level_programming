4d9de8debbca84f43275261337dab4962a70b123
dgquintero <dgquintero02@hotmail.com>
2020-04-16 01:12:37 +0000

    binary search fixed

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -46,7 +46,7 @@ int binary_search(int *array, size_t size, int value)
 		if (array[mid] == value)
 			return (mid);
 
-		if (array[mid] < value)
+		else if (array[mid] < value)
 			l = mid + 1;
 		else
 			r = mid - 1;
